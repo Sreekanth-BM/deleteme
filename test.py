d9409d6d0a31f86e90cf779f81081fb696bdfe80
Sreekanth-BM <sreekanthbudavaram@gmail.com>
2022-04-25 19:38:44 +0530

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ print("Added second line")
 
 p = argparse.ArgumentParser()
 
-p.add_argument('--var1',type='str', required=True, help='Just a var')
+p.add_argument('--var1', type=str, required=True, help='Just a var')
 
 args = p.parse_args()
 
